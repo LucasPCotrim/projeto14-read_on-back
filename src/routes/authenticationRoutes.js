06134d61d067f3bfe46c3fc2fb6cb31eab190614
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-15 00:19:00 -0300

    feat: Added POST (/sign-up) route

--- src/routes/authenticationRoutes.js
+++ src/routes/authenticationRoutes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { userSignUp } from '../controllers/authenticationController.js';
 
 const router = Router();
+
+router.post('/sign-up', userSignUp);
 
 export default router;
--- src/schemas/authenticationSchemas.js
+++ src/schemas/authenticationSchemas.js
@@ -1,1 +1,32 @@
 import joi from 'joi';
+
+const authSignUpSchema = joi.object({
+  name: joi.string().required(),
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+  passwordConfirm: joi.any().valid(joi.ref('password')).required(),
+  favGenres: joi
+    .array()
+    .items(
+      joi
+        .string()
+        .valid(
+          'Fantasia',
+          'Ficcção Cinetífica',
+          'Ação e Aventura',
+          'Romance',
+          'Suspense',
+          'Infantil',
+          'Biografia',
+          'História',
+          'Religião',
+          'Guias',
+          'Arte e Fotografia',
+          'Tecnologia e Ciência',
+          'Gastronomia',
+          'HQs'
+        )
+    ),
+});
+
+export { authSignUpSchema };
